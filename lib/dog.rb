ae943fc7faafdc8a7adf76b32dcc0b509e5999fe
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-01 05:05:45 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,14 +5,14 @@ class Dog
     @name = name
   end
 
-  def name
-    @name
-  end
+  # def name
+  #   @name
+  # end
+  #
+  # def name = (new_name)
+  #   @name = new_name
+  # end
+end
 
-  def name = (new_name)
-    @name = new_name
-  end
-end
-end
 
 fido = Dog.new("Fido")
