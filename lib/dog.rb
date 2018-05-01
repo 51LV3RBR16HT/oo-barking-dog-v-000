adb3e22abd6e23f8865f3e4e0993c2abe1015b7d
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-01 04:57:18 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,14 @@
 # Your code goes here!
 class Dog
 
-  def initialize(name)
-    @name = name
+  def initialize
+
   end
-  # 
+  #
   # def name=(name)
   #   @name = name
   # end
-  # 
+  #
   # # def name
   # #   name
   # # end
